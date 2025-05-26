ebe6aa9c9acd2733e4274f1d1c7122cc375f3d69
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2025-05-26 11:01:28 +0300

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 8f;
     private Rigidbody2D rigidbody2D;
 
+    private bool isGrounded = false;
+
     void Start() // Создаем метод Start (вызывается при старте игры)
     {
         // Получаем компонент Rigidbody2D из того объекта, на котором наложен скрипт
@@ -18,15 +20,30 @@ public class PlayerController : MonoBehaviour
     void Update() // Создаем метод Update (вызывается при каждом обновлении кадра)
     {
         direction = Input.GetAxis("Horizontal"); // Направление равно текущей нажатой клавише на оси
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
+            rigidbody2D.linearVelocity = new Vector2(rigidbody2D.linearVelocity.x, jumpForce);
         }
     }
 
     void FixedUpdate() // Создаем метод FixedUpdate (вызывается с фиксированной частотой)
     {
         // Устанавливаем векторную скорость для объекта
-        rigidbody2D.velocity = new Vector2(speed * direction, rigidbody2D.velocity.y);
+        rigidbody2D.linearVelocity = new Vector2(speed * direction, rigidbody2D.linearVelocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
